083ccba5c54856d306e17e9ad2c963b57a052983
ThairongSeng <seng.thairong@gmail.com>
2023-05-04 23:16:16 +0700

    this is my homework5 Next Js

---
+++ components/layout.js
@@ -0,0 +1,14 @@
+import Footer from "./footer"
+import NavBar from "./navbar"
+
+
+const Layout = ({children}) => {
+    return(
+        <>
+            <NavBar/>
+                <main>{children}</main>
+            <Footer/>
+        </>
+    )
+}
+export default Layout
